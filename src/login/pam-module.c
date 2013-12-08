fa32fb5165ea1be5c1edfdf3f686a4fcdc11c471
Dr. Werner Fink <werner@suse.de>
2013-12-08 23:10:35 +0000

    pam_systemd: Always reset XDG_RUNTIME_DIR.
    This fixes the 'su' case and not just 'su -' case previously.

--- src/login/pam-module.c
+++ src/login/pam-module.c
@@ -424,6 +424,12 @@ _public_ PAM_EXTERN int pam_sm_open_session(
                          pam_syslog(handle, LOG_ERR, "Failed to set runtime dir.");
                          goto finish;
                 }
+        } else {
+                (void) unsetenv("XDG_RUNTIME_DIR");
+                r = pam_putenv(handle, "XDG_RUNTIME_DIR");
+                if (r != PAM_SUCCESS && r != PAM_BAD_ITEM) {
+                         pam_syslog(handle, LOG_ERR, "Failed to unset runtime dir.");
+                }
         }
 
         if (!isempty(seat)) {
